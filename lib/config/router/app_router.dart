7404d617b217339b03cfae61a63eb310aefb7659
Diego Pagini <dhpagini@knowmadmood.com>
2024-01-04 17:17:16 +0100

    deep linking

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,16 +3,18 @@ import 'package:cine_app/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
   GoRoute(
-    path: '/',
-    name: HomeScreen.name,
-    builder: (context, state) => const HomeScreen(),
-  ),
-  GoRoute(
-      path: '/movie/:id',
-      name: MovieScreen.name,
-      builder: (context, state) {
-        final movieId = state.pathParameters['id'] ?? 'no-id';
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+      // Children routes
+      routes: [
+        GoRoute(
+            path: 'movie/:id',
+            name: MovieScreen.name,
+            builder: (context, state) {
+              final movieId = state.pathParameters['id'] ?? 'no-id';
 
-        return MovieScreen(movieId: movieId);
-      }),
+              return MovieScreen(movieId: movieId);
+            }),
+      ]),
 ]);
