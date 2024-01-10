394901544d3bbbae13e8e708b224251e95c4522d
Diego Pagini <dhpagini@knowmadmood.com>
2024-01-10 08:30:48 +0100

    router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,23 +1,58 @@
-import 'package:cine_app/presentation/views/home_views/favotires_view.dart';
 import 'package:go_router/go_router.dart';
+import 'package:cine_app/presentation/views/home_views/categories_view.dart';
 import 'package:cine_app/presentation/screens/screens.dart';
+import 'package:cine_app/presentation/views/home_views/home_view.dart';
+import 'package:cine_app/presentation/views/home_views/favotires_view.dart';
+
+//
+//Regular Router
+//final appRouter = GoRouter(initialLocation: '/', routes: [
+//   GoRoute(
+//       path: '/',
+//       name: HomeScreen.name,
+//       builder: (context, state) => const HomeScreen(
+//             childView: FavoritesView(),
+//           ),
+//       // Children routes
+//       routes: [
+//         GoRoute(
+//             path: 'movie/:id',
+//             name: MovieScreen.name,
+//             builder: (context, state) {
+//               final movieId = state.pathParameters['id'] ?? 'no-id';
+
+//               return MovieScreen(movieId: movieId);
+//             }),
+//       ]),
+// ]);
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(
-            childView: FavoritesView(),
-          ),
-      // Children routes
+  // Shell route to keep alive.
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
         GoRoute(
-            path: 'movie/:id',
-            name: MovieScreen.name,
-            builder: (context, state) {
-              final movieId = state.pathParameters['id'] ?? 'no-id';
+            path: '/',
+            builder: (context, state) => const HomeView(),
+            routes: [
+              GoRoute(
+                  path: 'movie/:id',
+                  name: MovieScreen.name,
+                  builder: (context, state) {
+                    final movieId = state.pathParameters['id'] ?? 'no-id';
 
-              return MovieScreen(movieId: movieId);
-            }),
-      ]),
+                    return MovieScreen(movieId: movieId);
+                  }),
+            ]),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) => const FavoritesView(),
+        ),
+        GoRoute(
+          path: '/categories',
+          builder: (context, state) => const CategoriesView(),
+        )
+      ])
 ]);
--- lib/presentation/widgets/shared/custom_botton_navigation.dart
+++ lib/presentation/widgets/shared/custom_botton_navigation.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   const CustomBottomNavigationBar({super.key});
 
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).matchedLocation;
+
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/categories');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(elevation: 0, items: const [
-      BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.label_outline), label: 'Categories'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.favorite_outline), label: 'Favorites'),
-    ]);
+    return BottomNavigationBar(
+        currentIndex: getCurrentIndex(context),
+        elevation: 0,
+        onTap: (value) {
+          // value == index.
+          onItemTapped(context, value);
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.label_outline), label: 'Categories'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline), label: 'Favorites'),
+        ]);
   }
 }
