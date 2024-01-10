2746c4290f84cac5a9e6c0ae5b35c9c3c93be632
Diego Pagini <dhpagini@knowmadmood.com>
2024-01-10 17:11:31 +0100

    open db

--- lib/infrastructure/datasources/isar_datasource.dart
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -1,7 +1,27 @@
+import 'package:isar/isar.dart';
 import 'package:cine_app/domain/entities/movie.dart';
 import 'package:cine_app/domain/datasources/local_storage_datasource.dart';
+import 'package:path_provider/path_provider.dart'
+    show getApplicationDocumentsDirectory;
 
 class IsarDatasource extends LocalStorageDatsource {
+  late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+    final dir = await getApplicationDocumentsDirectory();
+
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open([MovieSchema],
+          directory: dir.path, inspector: true);
+    }
+    // If is already opened
+    return Future.value(Isar.getInstance());
+  }
+
   @override
   Future<bool> isMovieFavorite(int movieId) {
     throw UnimplementedError();
